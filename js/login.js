feaca55d923d22376f2762bfa6720a6b2514e4c3
Pedro <pedrowilker1516@gmail.com>
2022-07-30 17:13:23 -0300

    create Memory game

---
+++ js/login.js
@@ -0,0 +1,8 @@
+const input= document.querySelector('login-input');
+const button= document.querySelector('button-login'); 
+
+const validateInput = ( ) => {
+    
+}
+
+input.addEventListener('input', validateInput);
